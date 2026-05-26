91283ce5b73d849fa901e20da9ca15ee4a05f50f
z-traveler <rsrnl_wysxr@163.com>
2026-05-26 21:19:26 +0800

    Enable bracket pair highlighting

--- nvim/lua/plugins/editor/blink-pairs.lua
+++ nvim/lua/plugins/editor/blink-pairs.lua
@@ -1,31 +1,31 @@
 local colors = require("config.ui").colors
 local update_hl = require("config.ui").update_hl
 
+local function apply_highlights()
+  update_hl("BlinkPairsRed", { fg = colors.red })
+  update_hl("BlinkPairsYellow", { fg = colors.yellow })
+  update_hl("BlinkPairsBlue", { fg = colors.blue })
+  update_hl("BlinkPairsOrange", { fg = colors.orange })
+  update_hl("BlinkPairsGreen", { fg = colors.green })
+  update_hl("BlinkPairsViolet", { fg = colors.violet })
+  update_hl("BlinkPairsCyan", { fg = colors.cyan })
+  update_hl("MatchParen", { bg = colors.dark_fg, fg = colors.light_green })
+end
+
 return {
   { "nvim-mini/mini.pairs", enabled = true },
   {
     "saghen/blink.pairs",
     version = "*",
-    enabled = false,
     dependencies = "saghen/blink.download",
     opts = function(_, opts)
-      vim.api.nvim_create_autocmd({ "User" }, {
-        pattern = "LazyLoad",
-        callback = function()
-          update_hl("BlinkPairsRed", { fg = colors.red })
-          update_hl("BlinkPairsYellow", { fg = colors.yellow })
-          update_hl("BlinkPairsBlue", { fg = colors.blue })
-          update_hl("BlinkPairsOrange", { fg = colors.orange })
-          update_hl("BlinkPairsGreen", { fg = colors.green })
-          update_hl("BlinkPairsViolet", { fg = colors.violet })
-          update_hl("BlinkPairsCyan", { fg = colors.cyan })
-          update_hl("MatchParen", { bg = colors.dark_fg, fg = colors.light_green })
-        end,
-        once = true,
+      apply_highlights()
+      vim.api.nvim_create_autocmd({ "ColorScheme" }, {
+        callback = apply_highlights,
       })
       local m_opts = {
         mappings = {
-          enabled = true,
+          enabled = false,
           disabled_filetypes = {},
           pairs = {},
         },
