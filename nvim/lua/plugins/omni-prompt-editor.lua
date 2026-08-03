ed3546636c562bd440bd87b4180af66d9bac7785
z-debian12 <rsrnl_wysxr@163.com>
2026-08-03 15:06:55 +0800

    Pin adaptive prompt editor runtime

--- nvim/lua/plugins/omni-prompt-editor.lua
+++ nvim/lua/plugins/omni-prompt-editor.lua
@@ -2,6 +2,7 @@ return {
   {
     "z-traveler/omni-hooks",
     name = "omni-prompt-editor",
+    commit = "46a9e6880f2e8fe95aadd7aa65fb7c0277d2ae28",
     lazy = false,
     config = function(plugin)
       vim.opt.runtimepath:prepend(plugin.dir .. "/modules/prompt-editor/nvim")
