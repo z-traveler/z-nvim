2a9dcdf4321bd039aebbb59afdd56e6141f27816
z-debian12 <rsrnl_wysxr@163.com>
2026-08-13 18:36:28 +0800

    fix(nvim): support prompt answers in Trouble

--- nvim/lua/plugins/omni-prompt-editor.lua
+++ nvim/lua/plugins/omni-prompt-editor.lua
@@ -27,8 +27,8 @@ return {
             and vim.api.nvim_buf_get_name(buf) == ""
           then
             local name = ("omni-prompt-answer-%d.md"):format(vim.fn.getpid())
-            vim.bo[buf].buftype = ""
             vim.api.nvim_buf_set_name(buf, vim.fs.joinpath(vim.fn.stdpath("run"), name))
+            vim.b[buf].omni_prompt_answer = true
             vim.lsp.start(marksman, { bufnr = buf })
           end
         end
--- nvim/lua/plugins/ui/trouble.lua
+++ nvim/lua/plugins/ui/trouble.lua
@@ -1,6 +1,24 @@
 return {
   {
     "folke/trouble.nvim",
+    config = function(_, opts)
+      require("trouble").setup(opts)
+      local main = require("trouble.view.main")
+      local valid = main._valid
+      main._valid = function(win, buf)
+        if
+          win
+          and buf
+          and vim.api.nvim_win_is_valid(win)
+          and vim.api.nvim_buf_is_valid(buf)
+          and vim.api.nvim_win_get_buf(win) == buf
+          and vim.b[buf].omni_prompt_answer
+        then
+          return true
+        end
+        return valid(win, buf)
+      end
+    end,
     keys = {
       { "<leader>xq", false },
       { "<leader>xx", false },
