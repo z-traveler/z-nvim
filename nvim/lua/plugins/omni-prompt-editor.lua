b31d62581733d19a5000f3106af2b45fcbca0158
z-debian12 <rsrnl_wysxr@163.com>
2026-08-25 16:12:44 +0800

    Update prompt editor for Codex 0.149

--- nvim/lua/plugins/omni-prompt-editor.lua
+++ nvim/lua/plugins/omni-prompt-editor.lua
@@ -2,7 +2,7 @@ return {
   {
     "z-traveler/omni-hooks",
     name = "omni-prompt-editor",
-    commit = "bd282d9832d8e079c240361292815b27b8bbc513",
+    commit = "2b750e5c3b97cb69635102fb5e28c9cac7a9d287",
     lazy = false,
     config = function(plugin)
       vim.opt.runtimepath:prepend(plugin.dir .. "/modules/prompt-editor/nvim")
